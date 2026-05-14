a5108cf0c74ba2e16d3d6bf5a4e1232dcff41c23
Allyson | Ghelplus <allyson@ghelplus.com.br>
2026-05-14 16:45:32 -0300

    Corrige configuracao de arquivos estaticos

--- fampertec/settings.py
+++ fampertec/settings.py
@@ -76,9 +76,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = "/media/"
 MEDIA_ROOT = Path(os.getenv("MEDIA_ROOT", BASE_DIR / "media"))
 
